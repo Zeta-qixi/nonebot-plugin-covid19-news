5a1a0795511b279f7df27a8ba8e5e6e126d071b4
Zeta-qixi <1019289695@qq.com>
2022-01-23 18:52:01 +0800

    nonebot2.0.0b1 版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='nonebot-plugin-covid19-news',
-    version='0.2.0',
+    version='0.2.0b',
     author='Zeta',
     author_email='',
     long_description="https://github.com/Zeta-qixi/nonebot-plugin-covid19-news",
